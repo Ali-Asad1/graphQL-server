3d09aac082a3a3c4037466401aebaf83cd0fc5db
Ali Asad <aliasad.3128@gmail.com>
2024-03-09 23:44:11 +0330

    chore: Add mock data for posts and authors

---
+++ database/_db.ts
@@ -0,0 +1,14 @@
+let posts = [
+  { id: "1", title: "Test title 1", body: "Non laborum non id voluptate minim." },
+  { id: "2", title: "Test title 2", body: "Non laborum non id voluptate minim." },
+  { id: "3", title: "Test title 3", body: "Non laborum non id voluptate minim." },
+  { id: "4", title: "Test title 4", body: "Non laborum non id voluptate minim." },
+  { id: "5", title: "Test title 5", body: "Non laborum non id voluptate minim." },
+];
+
+let authors = [
+  { id: "1", username: "Ali" },
+  { id: "2", username: "Tara" },
+];
+
+export default { posts, authors };
