3a68441d83a54b29e1b8727ff3d176974737cd5a
Ali Asad <aliasad.3128@gmail.com>
2024-03-10 23:04:37 +0330

    feat: Add `author_id` field to posts data structure

--- database/_db.ts
+++ database/_db.ts
@@ -1,9 +1,9 @@
 let posts = [
-  { id: "1", title: "Test title 1", body: "Non laborum non id voluptate minim." },
-  { id: "2", title: "Test title 2", body: "Non laborum non id voluptate minim." },
-  { id: "3", title: "Test title 3", body: "Non laborum non id voluptate minim." },
-  { id: "4", title: "Test title 4", body: "Non laborum non id voluptate minim." },
-  { id: "5", title: "Test title 5", body: "Non laborum non id voluptate minim." },
+  { id: "1", title: "Test title 1", body: "Non laborum non id voluptate minim.", author_id: "1" },
+  { id: "2", title: "Test title 2", body: "Non laborum non id voluptate minim.", author_id: "2" },
+  { id: "3", title: "Test title 3", body: "Non laborum non id voluptate minim.", author_id: "2" },
+  { id: "4", title: "Test title 4", body: "Non laborum non id voluptate minim.", author_id: "1" },
+  { id: "5", title: "Test title 5", body: "Non laborum non id voluptate minim.", author_id: "2" },
 ];
 
 let authors = [
